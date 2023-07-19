0feff0806c4f3bb15ac763c21331eb85b912752b
0xUncleEnzo <0xUncleEnzo@gmail.com>
2023-07-19 03:06:35 -0400

    Revert on zero price or rapid price change

--- src/PriceOracle.sol
+++ src/PriceOracle.sol
@@ -6,9 +6,14 @@ import {CToken, PriceOracle as IPriceOracle} from "@zoro-protocol/PriceOracle.so
 import {IPriceReceiver, PriceData} from "/IPriceReceiver.sol";
 
 contract PriceOracle is IPriceReceiver, IPriceOracle, Ownable {
+    uint256 constant MAX_DELTA_BASE = 1e18;
+    uint256 constant MAX_DELTA_MANTISSA = 20 * 1e16; // 20%
+
     mapping(CToken => PriceData) priceData;
 
     error InvalidTimestamp(uint256 timestamp);
+    error PriceIsZero();
+    error PriceExceededDelta(uint256 oldPrice, uint256 price);
 
     function setUnderlyingPrice(
         CToken cToken,
@@ -18,6 +23,7 @@ contract PriceOracle is IPriceReceiver, IPriceOracle, Ownable {
         PriceData storage oldData = priceData[cToken];
 
         _validateTimestamp(oldData, timestamp);
+        _validatePrice(oldData, price);
     }
 
     function getUnderlyingPrice(CToken cToken)
@@ -32,5 +38,16 @@ contract PriceOracle is IPriceReceiver, IPriceOracle, Ownable {
         pure
     {
         if (timestamp <= data.timestamp) revert InvalidTimestamp(timestamp);
+    }
+
+    function _validatePrice(PriceData memory data, uint256 price) private pure {
+        if (price == 0) revert PriceIsZero();
+
+        uint256 oldPrice = data.price;
+        uint256 delta = price > oldPrice ? price - oldPrice : oldPrice - price;
+        uint256 deltaMantissa = (oldPrice * MAX_DELTA_BASE) / delta;
+
+        if (deltaMantissa > MAX_DELTA_MANTISSA)
+            revert PriceExceededDelta(oldPrice, price);
     }
 }
