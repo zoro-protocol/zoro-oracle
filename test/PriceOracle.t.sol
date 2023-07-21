11fee74f42220db229c0cdd5ba4af5dc5bcc109a
0xUncleEnzo <0xUncleEnzo@gmail.com>
2023-07-21 19:16:07 -0400

    Test _validateLiveness

--- test/PriceOracle.t.sol
+++ test/PriceOracle.t.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.10;
 
 import {AggregatorV3Interface} from "chainlink/contracts/interfaces/AggregatorV3Interface.sol";
-import {InvalidTimestamp, PriceData, PriceOracleHarness as PriceOracle} from "/PriceOracleHarness.sol";
+import {CToken} from "@zoro-protocol/CToken.sol";
+import {FeedData} from "/IFeedRegistry.sol";
+import {InvalidTimestamp, PriceData, PriceIsStale, PriceOracleHarness as PriceOracle} from "/PriceOracleHarness.sol";
 import {Test} from "forge-std/Test.sol";
 
 contract PriceOracleTest is Test {
@@ -79,7 +81,7 @@ contract PriceOracleTest is Test {
             newPrice
         );
 
-        uint256 expected = 1 * 1e17; // 10%
+        uint256 expected = 1e17; // 10%
         assertEq(deltaMantissa, expected);
     }
 
@@ -111,5 +113,33 @@ contract PriceOracleTest is Test {
 
         uint256 expected = 0;
         assertEq(result, expected);
+    }
+
+    function test_validateLiveness_revertIfPriceIsStale() public {
+        CToken cToken = CToken(address(0));
+        uint256 livePeriod = 12 hours;
+        uint256 maxDeltaMantissa = 1e17; // 10%
+        FeedData memory fd = FeedData(cToken, livePeriod, maxDeltaMantissa);
+
+        uint256 timestamp = block.timestamp;
+        skip(livePeriod + 1); // Must be past the live period
+
+        vm.expectRevert(
+            abi.encodeWithSelector(PriceIsStale.selector, timestamp)
+        );
+        oracle.exposed_validateLiveness(fd, timestamp);
+    }
+
+    function test_validateLiveness_noRevertIfPriceIsLive() public {
+        CToken cToken = CToken(address(0));
+        uint256 livePeriod = 12 hours;
+        uint256 maxDeltaMantissa = 1e17; // 10%
+        FeedData memory fd = FeedData(cToken, livePeriod, maxDeltaMantissa);
+
+        uint256 timestamp = block.timestamp;
+
+        oracle.exposed_validateLiveness(fd, timestamp);
+
+        assertTrue(true, "Must not revert");
     }
 }
