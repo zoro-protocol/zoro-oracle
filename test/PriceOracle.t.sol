c23f5eacf25095e22527a8ada06aa681c97fc6bf
0xUncleEnzo <0xUncleEnzo@gmail.com>
2023-07-21 13:33:29 -0400

    Test calculateDeltaMantissa

--- test/PriceOracle.t.sol
+++ test/PriceOracle.t.sol
@@ -38,4 +38,48 @@ contract PriceOracleTest is Test {
         );
         oracle.exposed_validateTimestamp(pd, newTimestamp);
     }
+
+    function test_calculateDeltaMantissa_zeroIfNoPriceChange() public {
+        uint256 expectedDelta = 0;
+
+        uint256 oldPrice = 100;
+        uint256 newPrice = 100;
+
+        uint256 deltaMantissa = oracle.exposed_calculateDeltaMantissa(
+            oldPrice,
+            newPrice
+        );
+
+        assertEq(deltaMantissa, expectedDelta);
+    }
+
+    function test_calculateDeltaMantissa_positiveDeltaWhenNegativeChange()
+        public
+    {
+        uint256 oldPrice = 100;
+        uint256 newPrice = 90;
+
+        uint256 deltaMantissa = oracle.exposed_calculateDeltaMantissa(
+            oldPrice,
+            newPrice
+        );
+
+        uint256 expected = 1 * 1e17; // 10%
+        assertEq(deltaMantissa, expected);
+    }
+
+    function test_calculateDeltaMantissa_positiveDeltaWhenPositiveChange()
+        public
+    {
+        uint256 oldPrice = 100;
+        uint256 newPrice = 110;
+
+        uint256 deltaMantissa = oracle.exposed_calculateDeltaMantissa(
+            oldPrice,
+            newPrice
+        );
+
+        uint256 expected = 1 * 1e17; // 10%
+        assertEq(deltaMantissa, expected);
+    }
 }
