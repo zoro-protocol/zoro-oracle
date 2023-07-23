d90b6892e940ed31344ab1794de0ebecc41f926f
0xUncleEnzo <0xUncleEnzo@gmail.com>
2023-07-22 21:06:10 -0400

    Test _safeGetFeeData

--- test/PriceOracle.t.sol
+++ test/PriceOracle.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.10;
 import {AggregatorV3Interface} from "chainlink/contracts/interfaces/AggregatorV3Interface.sol";
 import {CToken} from "@zoro-protocol/CToken.sol";
 import {FeedData} from "/IFeedRegistry.sol";
-import {InvalidAddress, InvalidTimestamp, PriceData, PriceIsStale} from "/PriceOracle.sol";
+import {FeedNotConfigured, InvalidAddress, InvalidTimestamp, PriceData, PriceIsStale} from "/PriceOracle.sol";
 import {PriceOracleHarness as PriceOracle} from "/PriceOracleHarness.sol";
 import {Test} from "forge-std/Test.sol";
 
@@ -157,5 +157,51 @@ contract PriceOracleTest is Test {
         oracle.exposed_validateLiveness(fd, timestamp);
 
         assertTrue(true, "Must not revert");
+    }
+
+    function test_safeGetFeedData_revertIfFeedNotSet() public {
+        AggregatorV3Interface feed = AggregatorV3Interface(address(0));
+
+        vm.expectRevert(
+            abi.encodeWithSelector(FeedNotConfigured.selector, feed)
+        );
+        oracle.exposed_safeGetFeedData(feed);
+    }
+
+    function test_safeGetFeedData_revertIfCTokenNotSet() public {
+        address feedAddress = makeAddr("feed");
+        AggregatorV3Interface feed = AggregatorV3Interface(feedAddress);
+
+        address cTokenAddress = address(0);
+        uint256 livePeriod = 24 hours;
+        uint256 maxDeltaMantissa = 1e17; // 10%
+        oracle.workaround_setFeedData(
+            feed,
+            FeedData(CToken(cTokenAddress), livePeriod, maxDeltaMantissa)
+        );
+
+        vm.expectRevert(
+            abi.encodeWithSelector(FeedNotConfigured.selector, feed)
+        );
+        oracle.exposed_safeGetFeedData(feed);
+    }
+
+    function test_safeGetFeedData_returnFeedData() public {
+        address feedAddress = makeAddr("feed");
+        AggregatorV3Interface feed = AggregatorV3Interface(feedAddress);
+
+        address cTokenAddress = makeAddr("cToken");
+        uint256 livePeriod = 24 hours;
+        uint256 maxDeltaMantissa = 1e17; // 10%
+        oracle.workaround_setFeedData(
+            feed,
+            FeedData(CToken(cTokenAddress), livePeriod, maxDeltaMantissa)
+        );
+
+        FeedData memory fd = oracle.exposed_safeGetFeedData(feed);
+
+        assertEq(address(fd.cToken), cTokenAddress);
+        assertEq(fd.livePeriod, livePeriod);
+        assertEq(fd.maxDeltaMantissa, maxDeltaMantissa);
     }
 }
