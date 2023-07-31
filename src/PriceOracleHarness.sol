267ff9bee0ad1d3ab9655d86ed295141f78855f4
0xUncleEnzo <0xUncleEnzo@gmail.com>
2023-07-31 14:14:06 -0400

    Use full paths for foundry-zksync compatibility

--- src/PriceOracleHarness.sol
+++ src/PriceOracleHarness.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.10;
 
-import {AggregatorV3Interface, CToken, FeedData, PriceData, PriceOracle} from "/PriceOracle.sol";
+import {AggregatorV3Interface, CToken, FeedData, PriceData, PriceOracle} from "src/PriceOracle.sol";
 
 contract PriceOracleHarness is PriceOracle {
     constructor(
