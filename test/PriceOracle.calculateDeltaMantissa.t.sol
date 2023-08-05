de62296dd176cb84ae8753786d830b0582cca135
0xUncleEnzo <0xUncleEnzo@gmail.com>
2023-08-04 22:58:16 -0400

    Test overflow conditions for delta calc

--- test/PriceOracle.calculateDeltaMantissa.t.sol
+++ test/PriceOracle.calculateDeltaMantissa.t.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
+import {Math} from "lib/openzeppelin-contracts/contracts/utils/math/Math.sol";
+import {MAX_DELTA_BASE} from "src/PriceOracle.sol";
 import {PriceOracleHarness as PriceOracle} from "src/PriceOracleHarness.sol";
 import {Test} from "forge-std/Test.sol";
 
 contract CalculateDeltaMantissa is Test {
+    using Math for uint256;
+
     PriceOracle public oracle;
 
     function setUp() public {
@@ -17,6 +21,27 @@ contract CalculateDeltaMantissa is Test {
         uint256 oldPrice = 100;
         uint256 newPrice = 100;
 
+        uint256 deltaMantissa = oracle.exposed_calculateDeltaMantissa(
+            oldPrice,
+            newPrice
+        );
+
+        assertEq(deltaMantissa, expectedDelta);
+    }
+
+    function testFuzz_Overflow(uint256 oldPrice, uint256 newPrice) public {
+        vm.assume(oldPrice > 0);
+
+        uint256 percentChange = (newPrice.max(oldPrice) -
+            newPrice.min(oldPrice)) / oldPrice;
+
+        uint256 maxChangeBeforeOverflow = type(uint256).max / MAX_DELTA_BASE;
+
+        vm.assume(percentChange > maxChangeBeforeOverflow);
+
+        uint256 expectedDelta = 0;
+
+        vm.expectRevert();
         uint256 deltaMantissa = oracle.exposed_calculateDeltaMantissa(
             oldPrice,
             newPrice
