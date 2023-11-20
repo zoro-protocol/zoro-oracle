2446c770b9d1a04fe5ba0aa470142aa77f706a73
0xUncleEnzo <0xUncleEnzo@gmail.com>
2023-11-19 23:34:35 -0500

    Separate internal unit tests for price setter

--- test/PriceOracle.setUnderlyingPrice.t.sol
+++ test/PriceOracle.setUnderlyingPrice.t.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.10;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import {CToken} from "zoro-protocol/contracts/CToken.sol";
 import {FeedData} from "src/IFeedRegistry.sol";
-import {BasePriceOracle} from "src/BasePriceOracle.sol";
 import {PriceOracleHarness as PriceOracle} from "src/PriceOracleHarness.sol";
 import {Test} from "forge-std/Test.sol";
 
@@ -34,21 +32,6 @@ contract SetUnderlyingPrice is Test {
         oracle.setUnderlyingPrice(feed, price);
     }
 
-    function test_RevertIfNewPriceIsInvalid() public {
-        address feedAddress = makeAddr("feed");
-        AggregatorV3Interface feed = AggregatorV3Interface(feedAddress);
-
-        uint256 decimals = 8;
-        uint256 underlyingDecimals = 18;
-        FeedData memory fd = FeedData(feed, decimals, underlyingDecimals);
-
-        oracle.workaround_setFeedData(feed, fd);
-
-        uint256 price = 0; // $1 (8 decimals)
-        vm.expectRevert(BasePriceOracle.PriceIsZero.selector);
-        oracle.setUnderlyingPrice(feed, price);
-    }
-
     function test_EmitOnSuccess() public {
         address feedAddress = makeAddr("feed");
         AggregatorV3Interface feed = AggregatorV3Interface(feedAddress);
@@ -64,44 +47,5 @@ contract SetUnderlyingPrice is Test {
         vm.expectEmit();
         emit NewPrice(feed, price);
         oracle.setUnderlyingPrice(feed, price);
-    }
-
-    function test_SetFirstPriceData() public {
-        address feedAddress = makeAddr("feed");
-        AggregatorV3Interface feed = AggregatorV3Interface(feedAddress);
-
-        uint256 decimals = 8;
-        uint256 underlyingDecimals = 18;
-        FeedData memory fd = FeedData(feed, decimals, underlyingDecimals);
-
-        oracle.workaround_setFeedData(feed, fd);
-
-        uint256 price = 1e8; // $1 (8 decimals)
-        oracle.setUnderlyingPrice(feed, price);
-
-        uint256 result = oracle.exposed_prices(feed);
-
-        assertEq(result, price);
-    }
-
-    function test_UpdatePriceData() public {
-        address feedAddress = makeAddr("feed");
-        AggregatorV3Interface feed = AggregatorV3Interface(feedAddress);
-
-        uint256 decimals = 8;
-        uint256 underlyingDecimals = 18;
-        FeedData memory fd = FeedData(feed, decimals, underlyingDecimals);
-
-        oracle.workaround_setFeedData(feed, fd);
-
-        uint256 price = 1e8; // $1 (8 decimals)
-        oracle.setUnderlyingPrice(feed, price);
-
-        uint256 newPrice = 11e7; // $1.10
-        oracle.setUnderlyingPrice(feed, newPrice);
-
-        uint256 result = oracle.exposed_prices(feed);
-
-        assertEq(result, newPrice);
     }
 }
